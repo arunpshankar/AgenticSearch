7e4b08402f2bb1996ed7201a5a6a070dccc8d0d4
Arun Shankar <arunprshankar@gmail.com>
2025-01-06 10:42:46 -0600

    wip

--- src/config/setup.py
+++ src/config/setup.py
@@ -1,4 +1,6 @@
+import yaml
 import os
+
 
 # Configuration Constants
 BASE_DIR: str = os.path.dirname(os.path.abspath(__file__))
@@ -7,3 +9,11 @@ DATA_DIR: str = os.path.join(PROJECT_ROOT, 'data')
 IMAGES_DIR: str = os.path.join(PROJECT_ROOT, 'img')
 GOOGLE_ICON_PATH: str = os.path.join(IMAGES_DIR, 'google_logo.svg')
 TEMPLATES_DIR: str = os.path.join(PROJECT_ROOT, 'templates')
+
+def get_api_key():
+    # Load the YAML file
+    with open(os.path.join('credentials', 'api.yml'), 'r') as f:
+        config = yaml.safe_load(f)
+    
+    # Return the SERP_API_KEY value
+    return config.get('SERP_API_KEY')
